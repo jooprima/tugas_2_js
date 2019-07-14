f88afa197f42bf9693120631b5368753e21dd158
yohanes_prima <jooprima@gmail.com>
2019-07-14 22:29:27 +0700

    Tugas 2 js
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var umur = 28;
+var nama = "Yohanes";
+var x = 100;
+if (x > 90) {
+  x = true;
+} else {
+  x = false;
+}
+var y = null;
+var i = [3, 4, 2, 6, 5];
+
+console.log(umur);
+console.log(nama);
+console.log(x);
+console.log(y);
+console.log(i);
